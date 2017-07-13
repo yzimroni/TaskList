3923eb79876d2388346ff2b96e57e28288d1b769
yzimroni <yanay03@gmail.com>
2017-07-13 16:49:23 +0300

    Inital commit

---
+++ src/net/yzimroni/tasklist/TaskListPlugin.java
@@ -0,0 +1,17 @@
+package net.yzimroni.tasklist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TaskListPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
